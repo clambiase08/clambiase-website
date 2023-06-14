bc0809d2a4240b3339c109dd4ce2d22dfdd7dd9c
Christina Lambiase <christina.lambiase@gmail.com>
2023-06-13 20:13:05 -0700

    updated JS

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 
 
 
-const input1 = document.getElementById("submit");
+const input1 = document.querySelector("#submit");
 input1.addEventListener('click', function() {
     alert('Stay Tuned!');
 });
 
-const input2 = document.getElementById("explore");
+//Practicing arrow functions:
+//input1.addEventListener('click', () => alert('Stay Tuned!'));
+
+const input2 = document.querySelector("#explore");
 input2.addEventListener('click', function() {
     alert('Coming Soon!');
 });
+
+//Practicing adding event listener that deletes the submit button:
+
+//function deleteSubmit(e) {
+//    e.target.remove()
+//}
+
+//function addEventListenerToSubmitButton() {
+//    const submit = document.querySelector("#submit")
+//    submit.addEventListener('click', deleteSubmit)
+//}
+
+//addEventListenerToSubmitButton()
