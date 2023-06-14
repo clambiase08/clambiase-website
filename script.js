b62cb555da1430369221022257acb430a6c73119
Christina Lambiase <christina.lambiase@gmail.com>
2023-06-14 13:11:54 -0700

    updated JS based on DOM Events lecture to include submit listener for form

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 
 
 
-const input1 = document.querySelector("#submit");
-input1.addEventListener('click', function() {
-    alert('Stay Tuned!');
-});
+const form = document.querySelector('form');
 
-//Practicing arrow functions:
-//input1.addEventListener('click', () => alert('Stay Tuned!'));
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.message.value)
+    form.reset()
+}
+
+form.addEventListener('submit', handleSubmit);
+
+// const input1 = document.querySelector('#submit')
+// input1.addEventListener('click', () => alert('Stay Tuned!'));
 
 const input2 = document.querySelector("#explore");
 input2.addEventListener('click', function() {
