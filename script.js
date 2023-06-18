73e4dec6abb9cfa7559a79f2ff47552cb55f0c58
Christina Lambiase <christina.lambiase@gmail.com>
2023-06-18 15:58:03 -0700

    added db.json and worked on POST fetch

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 
 
 
-const form = document.querySelector('form');
+const contactForm = document.querySelector('form');
+contactForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(e) {
     e.preventDefault();
-    console.log(e.target.message.value)
-    form.reset()
+    let messageObj = {
+        name: e.target.name.value,
+        email: e.target.email.value,
+        reason: e.target.reason.value,
+        priority: e.target.priority.value,
+        message: e.target.message.value
+    }
+    contactForm.reset()
+    submitForm(messageObj)
 }
 
-form.addEventListener('submit', handleSubmit);
 
-// const input1 = document.querySelector('#submit')
-// input1.addEventListener('click', () => alert('Stay Tuned!'));
+function submitForm(messageObj) {
+    fetch('http://localhost:3000/messages', {
+        method: 'POST',
+        headers: {
+           'Content-type': 'application/json' 
+        },
+        body: JSON.stringify(messageObj)
+    })
+    .then(res => {
+        if (!res.ok) {throw new Error('Network response was not ok');
+     }
+     return res.json();
+    })
+    .then(message => console.log(message))
+    .catch(error => console.log(error))
+}
 
 const input2 = document.querySelector("#explore");
 input2.addEventListener('click', function() {
